fe3cb4b65a28fe724a6acaa08c43a8e7cfb1e07c
Khatri7968 <engrpawan111@gmail.com>
2022-12-11 20:52:33 +0500

    added common template files

--- flaskblog.py
+++ flaskblog.py
@@ -1,15 +1,32 @@
-from flask import Flask
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
+posts = [
+    {
+        'author': 'Programming with Marsh',
+        'title': 'Blog Post-1',
+        'content': 'First Post content',
+        'date_posted': 'May 21, 2019'
+    },
+
+    {
+        'author': 'Corey Achafer',
+        'title': 'Blog Post-2',
+        'content': '2nd Post content',
+        'date_posted': 'May 22, 2019'
+    },
+
+]
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return "<h1>Home Page</h1>"
+    return render_template('home.html', posts=posts)
 
 @app.route("/about")
 def about():
-    return "<h1>About Page</h1>"
+    return render_template('About.html', title = 'About')
 
 
 if __name__ == '__main__':
