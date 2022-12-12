9c48174fc777049bc8c47b17d419ff258d855b73
Khatri7968 <engrpawan111@gmail.com>
2022-12-12 12:43:39 +0500

    Added register.html & forms.py files

--- flaskblog.py
+++ flaskblog.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template
+from forms import RegristrationForm, LogInForm
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '684cce1ec4bcfbebaa14d2ff73a79fd3'
 
 posts = [
     {
@@ -27,6 +30,20 @@ def home():
 @app.route("/about")
 def about():
     return render_template('About.html', title = 'About')
+
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegristrationForm()
+    return render_template('register.html', title='Register', form=form)
+
+@app.route("/login")
+def login():
+    form = LogInForm()
+    return render_template('login.html', title='login', form=form)
+
+
+
 
 
 if __name__ == '__main__':
