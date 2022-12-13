1d4574a5556989a0ab6180808e67f03d6f94046c
Khatri7968 <engrpawan111@gmail.com>
2022-12-13 21:29:33 +0500

    Added files to resolve import errors

---
+++ flaskblog/routes.py
@@ -0,0 +1,53 @@
+from flask import render_template, url_for
+from flaskblog.forms import RegristrationForm, LogInForm
+from flask import flash
+from flask import redirect
+
+from flaskblog import app
+
+from flaskblog.models import User, Post
+
+posts = [
+    {
+        'author': 'Programming with Marsh',
+        'title': 'Blog Post-1',
+        'content': 'First Post content',
+        'date_posted': 'May 21, 2019'
+    },
+
+    {
+        'author': 'Corey Achafer',
+        'title': 'Blog Post-2',
+        'content': '2nd Post content',
+        'date_posted': 'May 22, 2019'
+    },
+
+]
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template('home.html', posts=posts)
+
+@app.route("/about")
+def about():
+    return render_template('About.html', title = 'About')
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegristrationForm()
+    if form.validate_on_submit():
+        flash(f'Acccount created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LogInForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data =="12345":
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash("Login Uncsuccessful. Please check username and password", 'danger')
+    return render_template('login.html', title='login', form=form)
---
+++ run.py
@@ -0,0 +1,33 @@
+from flaskblog import app
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+# Adding user to db
+# user_1 = User(username='Khatri7968', email='engrpawan111@gmail.com', password='password')
+# db.session.add(user_1)
+
+# Commit to db
+# db.seession.commit()
+
+# Shows all users means list of users
+# User.query.all()
+#
+# Getting first user
+# User.query.first()
+#
+# Getting specific user
+# User.query.filter_by(username='Khatri7968').all()
+#
+# It returns the user
+# User.query.get(1)
+#
+# Creating a post
+# post_1 = Post(title='Blog 1', content='First post content', user_id=user.id)
+# db.session.add(post_1)
+# user.posts
+
+# post = Post.query.first()
+# post.user_id
+# post.author
+#
